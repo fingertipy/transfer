596432a2e61afcb0f941e4f2c45c558ba9d54f03
fingertipy <fingertipy@126.com>
2019-12-11 18:54:19 +0800

    update script

--- src/main/resources/script/server.sh
+++ src/main/resources/script/server.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
+#service name
 app='transfer-1.0-SNAPSHOT.jar'
+#input args
 cmd=$1
+#pid
 pid=`ps -ef|grep java|grep $app|awk '{print $2}'`
 
+#funcation to start transfer service
 startup(){
   cat /opt/null > nohup.out
   nohup java -jar $app &
   tail -f nohup.out
 }
 
+#validate pramater
 if [ ! $cmd ]; then
   echo "Please specify args 'start|restart|stop'"
   exit
 fi
 
+#start service
 if [ $cmd == 'start' ]; then
   if [ ! $pid ]; then
     startup
@@ -22,6 +28,7 @@ if [ $cmd == 'start' ]; then
   fi
 fi
 
+#restart service
 if [ $cmd == 'restart' ]; then
   if [ $pid ]
     then
@@ -32,6 +39,7 @@ if [ $cmd == 'restart' ]; then
   startup  
 fi
 
+#stop service
 if [ $cmd == 'stop' ]; then
   if [ $pid ]; then
     echo "transfer service will be stop!"
